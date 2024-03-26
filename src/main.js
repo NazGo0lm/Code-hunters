e36c5e6586efd10aaf92a6f9697b5dc34a59ef07
Nazar Nazarko <starmok20@gmail.com>
2024-03-26 15:35:01 +0200

    add btn functionality

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+let burger_menu = document.querySelector(".modal-menu");
+let modal_btn = document.querySelector(".burger-menu");
+
+modal_btn.onclick = function () {
+    burger_menu.classList.toggle("is-open");
+    
+
+}
+document.onclick = function (e) {
+    if (burger_menu.contains(e.target)) {
+    
+    burger_menu.classList.remove("is-open");
+    
+
+}
+
+}
+
 
 
 
